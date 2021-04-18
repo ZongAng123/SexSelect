e31fcad228a7acc007f315862d4b0365f5441569
纵昂 <zongang15@yahoo.com>
2021-04-18 10:43:30 +0800

    Initial Commit

---
+++ SexSelect/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SexSelect
+//
+//  Created by 纵昂 on 2021/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SexSelect/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SexSelect
+//
+//  Created by 纵昂 on 2021/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
